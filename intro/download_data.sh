f673d55e353cc9f9e4532fb90a9b9feb8cc31388
Luke Hsiao <lwhsiao@stanford.edu>
2018-08-09 16:06:09 -0700

    Update supervision notebook

--- intro/download_data.sh
+++ intro/download_data.sh
@@ -1,6 +1,6 @@
 echo "Downloading tutorial databases..."
-url=http://i.stanford.edu/hazy/share/fonduer/intro_dbs.tar.gz
-data_tar=intro_dbs
+url=http://i.stanford.edu/hazy/share/fonduer/intro_dbs_v0.3.0.tar.gz
+data_tar=intro_dbs_v0.3.0
 if type curl &>/dev/null; then
     curl -RLO $url
 elif type wget &>/dev/null; then
